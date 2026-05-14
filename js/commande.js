7515f095e04f5185fd516bce7e239ae981c21fff
Edween Martinez <edweenmmartinez@outlook.com>
2026-05-13 22:53:46 -0400

    Ajout du js de client a commande mais avec adaptation

--- js/commande.js
+++ js/commande.js
@@ -1,26 +1,68 @@
 const commandesBody = document.getElementById("commande-body");
+const commandeForm = document.getElementById("formCommande");
+const reload = document.getElementById("bActualiser");
 
 async function chargerCommandes() {
 
-    const commandes = await getAll("commande");
+    try {
 
-    commandesBody.innerHTML = "";
+        const commandes = await getAll("commande");
 
-    commandes.forEach(commande => {
+        commandesBody.innerHTML = commandes.map(commande => `
 
-        commandesBody.innerHTML += `
-        
-        <tr>
-           
-             <td>${commande.id_commande}</td>
-    <td>${commande.date_commande}</td>
-    <td>${commande.prix_total}</td>
-    <td>${commande.id_client}</td>
+            <tr>
+                <td>${commande.id_commande}</td>
+                <td>${commande.date_commande}</td>
+                <td>${commande.prix_total}</td>
+                <td>${commande.id_client}</td>
 
-        </tr>
-        
-        `;
-    });
+                <td>
+                    <button class="bSupprimer" onclick="supprimerCommande(${commande.id_commande})">
+                        Supprimer
+                    </button>
+                </td>
+            </tr>
+
+        `).join('');
+
+    } catch (error) {
+
+        commandesBody.innerHTML = `<tr><td colspan="5">${error.message}</td></tr>`;
+
+    }
 }
+
+commandeForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const nouvelleCommande = {
+        id_commande: Number(document.getElementById("idCommande").value),
+        date_commande: document.getElementById("dateCommande").value+"T00:00:00Z",
+        prix_total: Number(document.getElementById("prixCommande").value),
+        id_client: Number(document.getElementById("idClientCommande").value)
+    };
+
+    try {
+        await create("commande", nouvelleCommande);
+        commandeForm.reset();
+        chargerCommandes();
+    } catch (error) {
+        alert(error.message);
+    }
+});
+
+async function supprimerCommande(id) {
+
+    if (!confirm(`Supprimer la commande ${id} ?`)) return;
+
+    try {
+        await remove("commande", id);
+        chargerCommandes();
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+reload.addEventListener("click", chargerCommandes);
 
 chargerCommandes();
